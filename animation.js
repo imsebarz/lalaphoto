0cac0dc6b50c3624343366405424af29641200d7
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2020-08-08 12:38:40 -0500

    Fix some bugs

--- animation.js
+++ animation.js
@@ -1,16 +1,10 @@
 import Swiper from "https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js";
 
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const nav = document.querySelector(".nav");
 const lala = document.querySelector(".lala");
-const imgwrapper = document.querySelector(".img-wrapper");
 const swipercontainer = document.querySelector(".swiper-container");
 const info = document.querySelector(".info");
 const infouls = document.querySelectorAll(".info ul");
 const clickme = document.querySelector(".clickme");
-const heroname = document.querySelector(".text h1");
-const herodescription = document.querySelector(".text p");
 const navlinks = document.querySelectorAll(".bottomnav ul li");
 const logo = document.querySelector(".nav .logo");
 const proyectos = document.querySelector(".proyects");
@@ -18,148 +12,156 @@ const profiletext = document.querySelector(".profiletext");
 const proyectoslinks = document.querySelectorAll(".proyects ul li");
 const textlist = document.querySelectorAll(".text ul");
 
-TweenMax.to(left, 2, { delay: 0.8, width: "50%", ease: Power2.easeInOut });
-TweenMax.to(right, 2, { delay: 0.6, width: "50%", ease: Power3.easeInOut });
-TweenMax.from(nav, 2, { delay: 0.8, opacity: 0, ease: Expo.easeInOut });
-TweenMax.from(heroname, 2, { delay: 0.6, x: 1000, ease: Circ.easeInOut });
-TweenMax.fromTo(
-  proyectos,
-  2,
-  { delay: 0.6, x: 1000, ease: Circ.easeInOut },
-  {
-    x: 1300,
-  }
-);
+// -------------------- Create timelines -----------------------------------
 
-TweenMax.to(lala, 1, { delay: 0.6, width: "60vw", ease: Power2.easeInOut });
+const tl = new TimelineMax();
+const lalaTl = new TimelineMax();
+const profileTl = new TimelineMax();
+const contactTl = new TimelineMax({ paused: true });
 
-navlinks[1].classList.add("navactive");
-proyectoslinks[0].classList.add("proyectactive");
+// -------------------- First load animation -----------------------------------
 
-TweenMax.from(herodescription, 2, {
-  delay: 0.7,
-  x: 1000,
-  ease: Circ.easeInOut,
-});
+tl.to(".right", 1, { width: "50%", ease: Power3.easeInOut })
+  .to(".left", 1, { width: "50%", ease: Power2.easeInOut }, "-=0.8")
+  .from(".nav", 1, { opacity: 0, ease: Expo.easeInOut }, "-=0.8")
+  .from(".text h1", 2, { x: 1000, ease: Circ.easeInOut }, "-=2")
+  .to(lala, 1, { width: "60vw", ease: Power2.easeInOut }, "-=1.8")
+  .from(
+    ".text p",
+    2,
+    {
+      x: 1000,
+      ease: Circ.easeInOut,
+    },
+    "-=1.7"
+  )
+  .from(
+    ".bottomnav ul li",
+    1,
+    {
+      x: 1000,
+      ease: Circ.easeInOut,
+    },
+    "-=1.5"
+  )
+  .from(
+    ".info",
+    2,
+    {
+      y: 1000,
+      ease: Circ.easeInOut,
+      onComplete: () => {
+        navlinks[1].classList.add("navactive");
+        proyectoslinks[0].classList.add("proyectactive");
+      },
+    },
+    "-=2.5"
+  );
 
-TweenMax.staggerFrom(
-  ".bottomnav ul li",
-  2,
-  {
-    delay: 1,
-    x: 1000,
-    ease: Circ.easeInOut,
-  },
-  0.08
-);
+// -------------------- First load animation -----------------------------------
 
-TweenMax.from(info, 2, { delay: 1.5, y: 100, ease: Circ.easeInOut });
+// -------------------- HeroImg onClick listener  -----------------------------------
 
-lala.addEventListener("click", () => {
+function lalaclick() {
   navlinks[1].classList.add("navactive");
   navlinks[0].classList.remove("navactive");
   navlinks[2].classList.remove("navactive");
-  TweenMax.to(heroname, 1, { x: 1000, ease: Circ.easeInOut });
-  TweenMax.to(herodescription, 1, {
-    delay: 0.2,
-    x: 1000,
-    ease: Circ.easeInOut,
-  });
+  lalaTl
+    .to(".text h1", 1, { x: 1000, ease: Circ.easeInOut })
+    .to(
+      ".text p",
+      1,
+      {
+        x: 1000,
+        ease: Power3.easeInOut,
+      },
+      "-=0.8"
+    )
+    .to(
+      profiletext,
+      1,
+      {
+        opacity: 0,
+        ease: Circ.easeInOut,
+      },
+      "-=1.4"
+    )
+    .to(
+      textlist,
+      1,
+      {
+        display: "block",
+        ease: Circ.easeInOut,
+      },
+      "-=1.6"
+    )
+    .to(
+      lala,
+      0.5,
+      {
+        x: -1200,
+        ease: Power2.easeInOut,
+      },
+      "-=1.5"
+    )
+    .fromTo(
+      swipercontainer,
+      1,
+      { x: -1400, ease: Circ.easeInOut },
+      {
+        x: -200,
+        scale: 0.8,
+        y: -60,
+        ease: Circ.easeInOut,
+        onComplete: () => {
+          lala.style.cursor = "grab";
+        },
+      },
+      "-=0.5"
+    )
+    .fromTo(
+      proyectos,
+      1,
+      { x: 1200, ease: Circ.easeInOut },
+      {
+        x: 0,
+        onComplete: () => {
+          proyectos.style.display = "block";
+        },
+      },
+      "-=1"
+    );
+  lalaTl.play();
+}
 
-  lala.style.cursor = "grab";
+lala.addEventListener("click", lalaclick);
 
-  TweenMax.to(profiletext, 1, {
-    delay: 0.2,
-    opacity: 0,
-    ease: Circ.easeInOut,
-  });
+// -------------------- HeroImg onClick listener  -----------------------------------
 
-  TweenMax.to(textlist, 1, {
-    delay: 0.3,
-    display: "block",
-    ease: Circ.easeInOut,
-  });
-
-  TweenMax.fromTo(
-    swipercontainer,
-    1,
-    { delay: 0.3, x: -1400, ease: Circ.easeInOut },
-    {
-      delay: 0.3,
-      x: -200,
-      scale: 0.8,
-      y: -60,
-      ease: Circ.easeInOut,
-    }
-  );
-  TweenMax.to(lala, 0.5, {
-    delay: 0.1,
-    x: -1200,
-    ease: Power2.easeInOut,
-  });
-
-  TweenMax.fromTo(
-    proyectos,
-    2,
-    { delay: 0.8, x: 1200, ease: Circ.easeInOut },
-    {
-      x: 0,
-    }
-  );
-  proyectos.style.display = "block";
-});
+// -------------------- logo onClick listener  -----------------------------------
 
 logo.addEventListener("click", () => {
+  if (navlinks[0].classList.contains("navactive")) {
+    profileTl.reverse(-0.5);
+    lala.addEventListener("click", lalaclick);
+    lala.addEventListener("mousemove", mousemove);
+    lala.style.cursor = "none";
+  } else {
+    lalaTl.reverse(-0.5);
+  }
   navlinks[1].classList.add("navactive");
   navlinks[0].classList.remove("navactive");
   navlinks[2].classList.remove("navactive");
-
-  TweenMax.to(heroname, 1, { x: 0, y: 0, ease: Circ.easeInOut });
-  TweenMax.to(herodescription, 1, {
-    delay: 0.2,
-    x: 0,
-    y: 0,
-    ease: Circ.easeInOut,
-  });
-  TweenMax.to(lala, 1, { delay: 0.1, y: 0, scale: 1, ease: Power2 });
-  TweenMax.to(proyectos, 1, { delay: 0.3, x: 1000, ease: Circ.easeInOut });
-  lala.style.cursor = "none";
-  TweenMax.to(lala, 1, { delay: 0.1, x: -80, y: -30, ease: Power2 });
-  TweenMax.to(swipercontainer, 1, {
-    x: -1200,
-  });
-  clickme.textContent = "Click me";
-  proyectos.style.display = "none";
-  TweenMax.to(profiletext, 1, {
-    delay: 0.2,
-    opacity: 0,
-    ease: Circ.easeInOut,
-  });
 });
+
+// -------------------- logo onClick listener  -----------------------------------
 
 navlinks[1].addEventListener("click", () => {
   navlinks[1].classList.add("navactive");
-  TweenMax.to(heroname, 1, { x: 0, y: 0, ease: Circ.easeInOut });
-  TweenMax.to(herodescription, 1, {
-    delay: 0.2,
-    x: 0,
-    y: 0,
-    ease: Circ.easeInOut,
-  });
-  TweenMax.to(lala, 1, { delay: 0.1, y: 0, scale: 1, ease: Power2 });
-  TweenMax.to(proyectos, 1, { delay: 0.3, x: 1000, ease: Circ.easeInOut });
+  profileTl.reverse();
+  lala.addEventListener("click", lalaclick);
+  lala.addEventListener("mousemove", mousemove);
   lala.style.cursor = "none";
-  TweenMax.to(lala, 1, { delay: 0.1, x: -80, y: -30, ease: Power2 });
-  TweenMax.to(swipercontainer, 1, {
-    x: -1200,
-  });
-  clickme.textContent = "Click me";
-  proyectos.style.display = "none";
-  TweenMax.to(profiletext, 1, {
-    delay: 0.2,
-    opacity: 0,
-    ease: Circ.easeInOut,
-  });
 });
 
 const swiper = new Swiper(".swiper-container", {
@@ -173,14 +175,16 @@ const swiper = new Swiper(".swiper-container", {
   },
 });
 
-lala.addEventListener("mousemove", (e) => {
+function mousemove(e) {
   const pageX = e.screenX + "px";
   const pageY = e.clientY + "px";
   clickme.style.opacity = 1;
   clickme.style.left = pageX;
   clickme.style.top = pageY;
   clickme.textContent = "Click me";
-});
+}
+
+lala.addEventListener("mousemove", mousemove);
 
 lala.addEventListener("mouseout", (e) => {
   clickme.style.opacity = 0;
@@ -227,85 +231,92 @@ navlinks.forEach((link) => {
 });
 
 navlinks[0].addEventListener("click", () => {
-  TweenMax.to(lala, 0.5, {
-    delay: 0.1,
-    x: -200,
-    width: "750px",
-    ease: Power2.easeInOut,
-  });
-  TweenMax.to(proyectos, 1, { delay: 0.3, x: 1000, ease: Circ.easeInOut });
-  TweenMax.to(swipercontainer, 1, {
-    x: -1200,
-  });
-
-  TweenMax.to(heroname, 1, { x: 0, y: -200, ease: Circ.easeInOut });
-  TweenMax.to(herodescription, 1, {
-    delay: 0.2,
-    y: 500,
-    x: 0,
-    ease: Circ.easeInOut,
-  });
-  TweenMax.to(profiletext, 1, {
-    delay: 0.2,
-    opacity: 1,
-    ease: Circ.easeInOut,
-  });
+  profileTl.play();
+  profileTl
+    .to(lala, 0.5, {
+      x: -200,
+      width: "750px",
+      ease: Power2.easeInOut,
+    })
+    .to(proyectos, 0.5, { x: 1000, ease: Power3.easeInOut }, "-=1.3")
+    .to(swipercontainer, 0.5, {
+      x: -1200,
+    })
+    .to(".text h1", 1, { x: 0, y: -200, ease: Power3.easeInOut }, "-=1")
+    .to(
+      ".text p",
+      0.5,
+      {
+        y: 500,
+        x: 0,
+        ease: Power3.easeInOut,
+      },
+      "-=1.4"
+    )
+    .to(
+      profiletext,
+      0.5,
+      {
+        opacity: 1,
+        ease: Power2.easeInOut,
+      },
+      "-=1"
+    );
+  lala.removeEventListener("click", lalaclick);
+  lala.removeEventListener("mousemove", mousemove);
   profiletext.style.display = "block";
   proyectos.style.display = "none";
+  lala.style.cursor = "default";
 });
 
 infouls[1].style.display = "none";
 navlinks[2].addEventListener("click", () => {
-  TweenMax.to(info, 1, {
-    delay: 0.2,
-    height: "15em",
-    width: "40em",
-    ease: Circ.easeInOut,
-  });
-  TweenMax.fromTo(
+  contactTl.play();
+  const abajo = document.querySelector(".abajo");
+});
+
+const quit = () => {
+  window.removeEventListener("click", quit);
+  contactTl.reverse().timeScale(4);
+};
+
+contactTl
+  .fromTo(
+    ".info",
+    1,
+    {
+      background: "rgba(0, 0, 0, 0.8)",
+    },
+    {
+      height: "30%",
+      width: "35%",
+      ease: Circ.easeInOut,
+      zIndex: 10,
+      background: "rgba(0, 0, 0, 0.9)",
+    },
+    "-=0.5"
+  )
+  .to(
     infouls[0],
     2,
     {
       ease: Circ.easeInOut,
-      flexDirection: "row",
-    },
-    {
-      delay: 0.5,
-      ease: Circ.easeInOut,
       flexDirection: "column",
       display: "block",
-    }
-  );
-  TweenMax.fromTo(
+      width: "60%",
+    },
+    "-=1-2"
+  )
+  .to(
     infouls[1],
     2,
     {
       ease: Circ.easeInOut,
-      flexDirection: "row",
-    },
-    {
-      delay: 0.5,
-      ease: Circ.easeInOut,
       flexDirection: "column",
       display: "block",
-    }
+      onStart: () => {
+        window.addEventListener("click", quit);
+      },
+    },
+    "-=1.2"
   );
-});
-
-const cerrarinfo = () => {
-  TweenMax.to(info, 1, {
-    delay: 0.2,
-    height: "auto",
-    width: "auto",
-    ease: Circ.easeInOut,
-  });
-  TweenMax.to(infouls[0], 2, {
-    delay: 0.5,
-    ease: Circ.easeInOut,
-    display: "block",
-  });
-  TweenMax.to(infouls[1], 2, {
-    ease: Circ.easeInOut,
-    display: "none",
-  });
-};
